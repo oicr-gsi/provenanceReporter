a1ea6951d7c177b085d1420a7047e0d572162ac8
Richard Jovelin <RJovelin@oicr.on.ca>
2023-04-03 15:48:51 -0400

    added column with platform, renamed identifiers

--- provenance_reporter_app.py
+++ provenance_reporter_app.py
@@ -77,13 +77,69 @@ def group_sequences(L):
             fileprefix = os.path.basename(file1)
             fileprefix = '_'.join(fileprefix.split('_')[:-1])
             d = {'case': case1, 'sample': sample1, 'library': library1, 'run': run1,
-                 'files': [file1, file2], 'read_count': readcount, 'workflow': workflow1,
-                 'release_status': status1, 'ticket': ticket1, 'prefix':fileprefix}
+                  'files': [file1, file2], 'read_count': readcount, 'workflow': workflow1,
+                  'release_status': status1, 'ticket': ticket1, 'prefix':fileprefix}
             F.append(d)
        
     F.sort(key = lambda x: x['case'])
      
     return F
+
+
+
+def get_sequences(L):
+    '''
+    (list) -> list
+
+    Returns a list sequence file information by grouping paired fastqs    
+    Pre-condition: all fastqs are paired-fastqs. Non-paired-fastqs are discarded.
+    
+    Parameters
+    ----------
+    - L (list): List of sqlite3.Row extracted from the database and containing sequence file information
+    '''
+    
+    # sort list according to files
+    L.sort(key = lambda x: x['file'])
+    
+    F = []
+    
+    for i in range(len(L)):
+        # keep only read1
+        if json.loads(L[i]['attributes'])['read_number'] == '1':
+            case = L[i]['sample']
+            sample = L[i]['ext_id']
+            library =  L[i]['library']
+            group_id = L[i]['group_id']
+            workflow = L[i]['workflow'] + '_' + L[i]['version']
+            wfrun = L[i]['wfrun_id']
+            file = L[i]['file']
+            run = L[i]['run'] + '_' + str(L[i]['lane'])
+            platform = L[i]['platform']
+            status = L[i]['status']
+            ticket = L[i]['ticket']
+            read_count = json.loads(L[i]['attributes'])['read_count'] if 'read_count' in json.loads(L[i]['attributes']) else 'NA' 
+            
+            if 'GDR' in ticket:
+                ticket = os.path.join('https://jira.oicr.on.ca/browse/', os.path.basename(ticket))    
+            else:
+                ticket = ''
+            readcount = '{:,}'.format(int(read_count)) if read_count != 'NA' else 'NA'
+            fileprefix = os.path.basename(file)
+            fileprefix = '_'.join(fileprefix.split('_')[:-1])
+            d = {'case': case, 'sample': sample, 'library': library, 'run': run,
+                 'read_count': readcount, 'workflow': workflow, 'release_status': status,
+                 'ticket': ticket, 'prefix':fileprefix, 'platform': platform, 'group_id': group_id}
+            F.append(d)
+       
+    F.sort(key = lambda x: x['case'])
+     
+    return F
+
+
+
+
+
 
 
 def get_library_design(library_source):
@@ -746,7 +802,9 @@ def sequencing(project_name):
                          AND LOWER(Files.workflow) in ('casava', 'bcl2fastq', 'fileimportforanalysis', 'fileimport', 'import_fastq');".format(project_name)).fetchall()
     conn.close()
     # find and group pairs of fastqs
-    sequences = group_sequences(files)
+    #sequences = group_sequences(files)
+
+    sequences = get_sequences(files)
 
     return render_template('sequencing.html', routes=routes, project=project, sequences=sequences, pipelines=pipelines)
 
