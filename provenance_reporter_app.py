62ed917d5c221953d61403865968df561b84a62b
Richard Jovelin <richard.jovelin@oicr.on.ca>
2023-08-14 16:20:36 -0400

    re-organized WGS functions

--- provenance_reporter_app.py
+++ provenance_reporter_app.py
@@ -26,7 +26,7 @@ import base64
 
 from utilities import connect_to_db, get_children_workflows, filter_out_QC_workflows, \
     get_miso_sample_link, get_pipelines
-from whole_genome import get_bmpp_case, get_case_call_ready_samples, group_normal_tumor_pairs, \
+from whole_genome import get_call_ready_cases, get_bmpp_case, get_case_call_ready_samples, group_normal_tumor_pairs, \
     find_analysis_blocks, map_workflows_to_sample_pairs, map_workflows_to_parent, list_block_workflows, \
     get_block_analysis_date, sort_call_ready_samples, get_block_workflow_file_count, get_block_release_status, \
     get_amount_data, is_block_complete, order_blocks, name_WGS_blocks, create_block_json    
@@ -40,73 +40,73 @@ from sequencing import get_sequences, collect_sequence_info
    
 
 
-def get_call_ready_cases(project_name, platform, library_type):
-    '''
-    (str, str, str) -> dict
+# def get_call_ready_cases(project_name, platform, library_type):
+#     '''
+#     (str, str, str) -> dict
 
-    Returns a dictionary with samples and libraries and bmpp and downstream workflow ids for each case in a project,
-    restricting data to specified platform and library type
+#     Returns a dictionary with samples and libraries and bmpp and downstream workflow ids for each case in a project,
+#     restricting data to specified platform and library type
 
-    Parameters
-    ----------
-    - project_name (str): Name of the project
-    - platform (str): Name of sequencing platform.
-                      Accepted values: novaseq, nextseq, hiseq, miseq
-    - library_type (str): 2 letters-code indicating the type of library                   
-    '''
+#     Parameters
+#     ----------
+#     - project_name (str): Name of the project
+#     - platform (str): Name of sequencing platform.
+#                       Accepted values: novaseq, nextseq, hiseq, miseq
+#     - library_type (str): 2 letters-code indicating the type of library                   
+#     '''
 
-    # get all the samples for project name 
-    conn = connect_to_db()
-    data = conn.execute("SELECT Libraries.library, Libraries.sample, Libraries.project_id, \
-                          Libraries.ext_id, Libraries.group_id, Libraries.library_type, \
-                          Libraries.tissue_type, Libraries.tissue_origin, \
-                          Workflows.wf, Workflows.wfrun_id, Workflow_Inputs.platform \
-                          from Workflow_Inputs JOIN Libraries JOIN Workflows \
-                          WHERE Libraries.project_id = '{0}' AND Workflow_Inputs.project_id = '{0}' \
-                          AND Workflows.project_id = '{0}' AND Workflow_Inputs.wfrun_id = Workflows.wfrun_id \
-                          AND Workflow_Inputs.library = Libraries.library AND Libraries.library_type = '{1}';".format(project_name, library_type)).fetchall()
-    conn.close()
+#     # get all the samples for project name 
+#     conn = connect_to_db()
+#     data = conn.execute("SELECT Libraries.library, Libraries.sample, Libraries.project_id, \
+#                           Libraries.ext_id, Libraries.group_id, Libraries.library_type, \
+#                           Libraries.tissue_type, Libraries.tissue_origin, \
+#                           Workflows.wf, Workflows.wfrun_id, Workflow_Inputs.platform \
+#                           from Workflow_Inputs JOIN Libraries JOIN Workflows \
+#                           WHERE Libraries.project_id = '{0}' AND Workflow_Inputs.project_id = '{0}' \
+#                           AND Workflows.project_id = '{0}' AND Workflow_Inputs.wfrun_id = Workflows.wfrun_id \
+#                           AND Workflow_Inputs.library = Libraries.library AND Libraries.library_type = '{1}';".format(project_name, library_type)).fetchall()
+#     conn.close()
     
-    cases = {}
-    for i in data:
-        # select bmpp data sequenced on novaseq
-        if platform in i['platform'].lower():
-            if i['sample'] not in cases:
-                cases[i['sample']] = {'project': i['project_id'], 'samples': [], 'libraries': [], 'bmpp': []}
+#     cases = {}
+#     for i in data:
+#         # select bmpp data sequenced on novaseq
+#         if platform in i['platform'].lower():
+#             if i['sample'] not in cases:
+#                 cases[i['sample']] = {'project': i['project_id'], 'samples': [], 'libraries': [], 'bmpp': []}
             
-            sample = '_'.join([i['sample'], i['tissue_type'], i['tissue_origin'], i['library_type'], i['group_id']]) 
-            cases[i['sample']]['samples'].append(sample)
+#             sample = '_'.join([i['sample'], i['tissue_type'], i['tissue_origin'], i['library_type'], i['group_id']]) 
+#             cases[i['sample']]['samples'].append(sample)
                        
-            cases[i['sample']]['libraries'].append(i['library'])
-            if 'bammergepreprocessing' in i['wf'].lower():
-                cases[i['sample']]['bmpp'].append(i['wfrun_id'])
+#             cases[i['sample']]['libraries'].append(i['library'])
+#             if 'bammergepreprocessing' in i['wf'].lower():
+#                 cases[i['sample']]['bmpp'].append(i['wfrun_id'])
             
-    for i in cases:
-        cases[i]['samples'] = list(set(cases[i]['samples']))
-        cases[i]['libraries'] = list(set(cases[i]['libraries']))
-        cases[i]['bmpp'] = list(set(cases[i]['bmpp']))
+#     for i in cases:
+#         cases[i]['samples'] = list(set(cases[i]['samples']))
+#         cases[i]['libraries'] = list(set(cases[i]['libraries']))
+#         cases[i]['bmpp'] = list(set(cases[i]['bmpp']))
 
-    # find the bmpp downstream workflows
-    for i in cases:
-        downstream = []
-        for j in cases[i]['bmpp']:
-            d = get_children_workflows(cases[i]['project'], j)
-            # filter out QC workflows
-            d = filter_out_QC_workflows(cases[i]['project'], d)
-            # list all downstream workflows
-            downstream_wf = [k for m in d.values() for k in m]
-            downstream.extend(downstream_wf)
-            # get downstream workflows of dowmstream workflows
-            for k in downstream_wf:
-                d = get_children_workflows(cases[i]['project'], k)
-                # filter out QC workflows
-                d = filter_out_QC_workflows(cases[i]['project'], d)
-                df = [n for m in d.values() for n in m]
-                downstream.extend(df)
-        cases[i]['downstream'] = list(set(downstream)) 
+#     # find the bmpp downstream workflows
+#     for i in cases:
+#         downstream = []
+#         for j in cases[i]['bmpp']:
+#             d = get_children_workflows(cases[i]['project'], j)
+#             # filter out QC workflows
+#             d = filter_out_QC_workflows(cases[i]['project'], d)
+#             # list all downstream workflows
+#             downstream_wf = [k for m in d.values() for k in m]
+#             downstream.extend(downstream_wf)
+#             # get downstream workflows of dowmstream workflows
+#             for k in downstream_wf:
+#                 d = get_children_workflows(cases[i]['project'], k)
+#                 # filter out QC workflows
+#                 d = filter_out_QC_workflows(cases[i]['project'], d)
+#                 df = [n for m in d.values() for n in m]
+#                 downstream.extend(df)
+#         cases[i]['downstream'] = list(set(downstream)) 
     
     
-    return cases
+#     return cases
 
 
 
